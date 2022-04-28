3039260833be6bc486fd9279ed70d29ed6e757ef
vlad-bstrv <keocera@icloud.com>
2022-04-28 21:33:47 +0300

    add viewBinding

--- app/src/main/java/com/vladbstrv/gitapp/MainActivity.kt
+++ app/src/main/java/com/vladbstrv/gitapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.vladbstrv.gitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vladbstrv.gitapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
